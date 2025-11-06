7820452f0eb532d73f4ef7d7e60b1ebc696d20df
marieltdr <157079066+marieltdr@users.noreply.github.com>
2025-11-05 22:59:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,28 @@ const map = new mapboxgl.Map({
             type: 'geojson',
             data: 'https://raw.githubusercontent.com/marieltdr/183geodata/refs/heads/main/183data.geojson',
             generateId: true,
-            cluster: true, 
-            clusterMaxZoom: 12, 
-            clusterRadius: 50 
+            cluster: true, // Enable clustering
+            clusterMaxZoom: 12, // Max zoom level to cluster points on
+            clusterRadius: 50 // Radius in pixels to use for clustering
       });
 
         map.addLayer({
         id: 'clusters',
         type: 'circle',
         source: 'points-data',
-        filter: ['has', 'point_count'], 
+        filter: ['has', 'point_count'], // Filter for cluster features
         paint: {
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#51bbd6', 
-                100, '#f1f075', 
-                750, '#f28cb1' 
+                '#51bbd6', // Color for clusters with less than 100 points
+                100, '#f1f075', // Color for clusters with 100-750 points
+                750, '#f28cb1' // Color for clusters with 750+ points
             ],
             'circle-radius': [
                 'step',
                 ['get', 'point_count'],
-                20, 100, 30, 750, 40 
+                20, 100, 30, 750, 40 // Radius based on point count
             ]
         }
     });
@@ -60,28 +60,6 @@ const map = new mapboxgl.Map({
                 'circle-stroke-width': 1,
                 'circle-stroke-color': '#fff',
                 'circle-emissive-strength': 1
-            }
-        });
-
-        map.addInteraction('click-clusters', {
-            type: 'click',
-            target: { layerId: 'clusters' },
-            handler: (e) => {
-                const features = map.queryRenderedFeatures(e.point, {
-                    layers: ['clusters']
-                });
-                const clusterId = features[0].properties.cluster_id;
-                map.getSource('earthquakes').getClusterExpansionZoom(
-                    clusterId,
-                    (err, zoom) => {
-                        if (err) return;
-
-                        map.easeTo({
-                            center: features[0].geometry.coordinates,
-                            zoom: zoom
-                        });
-                    }
-                );
             }
         });
 
