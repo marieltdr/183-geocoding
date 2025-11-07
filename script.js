42f763fd3d6f10775ab4d91fe5efd4f9e4c5c2bf
marieltdr <157079066+marieltdr@users.noreply.github.com>
2025-11-06 18:16:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaWVsdGRyIiwiYSI6ImNtaDljY3lnNjA1NzIya3Bwbmw4YThndHMifQ.8xjaPLZ8qauEqwsk7tsrLw';
 const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/marieltdr/cmh9cfchm009g01ra3lh24uro', // Use the standard style for the map
-        zoom: 12, // initial zoom level, 0 is the world view, higher values zoom in
-        center: [-122.275, 37.871] // center the map on this longitude and latitude
+        style: 'mapbox://styles/marieltdr/cmh9cfchm009g01ra3lh24uro', 
+        zoom: 12, 
+        center: [-122.275, 37.871] 
      });
      
   map.on('load', function() {
